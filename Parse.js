72f4152a299740b7b22986e885664362870e256a
Lovelyn Tijesunimi-Israel <lovelynisrael@hotmail.com>
2015-07-07 17:40:59 +0100

    Code added

---
+++ Parse.js
@@ -0,0 +1,24 @@
+typeof 5;
+x = "10";
+typeof x;
+x = [1,"two","2",3];
+typeof x
+y = [typeof x[0], typeof x[1],typeof x[2],typeof x[3]]
+x = 457
+y = "457";
+x.length;
+y.length;
+x+y;
+val = parseInt(y) + x;
+parseInt ("45j");
+x = "L457K";
+parseInt(x);
+var x = "L457K";
+x.substring(1,4);
+var numberPlate = x.substring(1,4);
+typeof numberPlate;
+function parsePlate(x){
+    console.log (x.substring(1,4));
+}
+parsePlate("Q556T");
+
